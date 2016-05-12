d94162052510cdc8ae3a9cb5c5bfd03b2e54a9cb
jackist <jack@example.com>
2016-05-12 17:39:18 +0800

    1.add two packages 2.add BaseActivity

---
+++ app/src/main/java/com/jackist/fishweather/base/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.jackist.fishweather.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jackist on 2016/5/12.
+ */
+public class BaseActivity extends AppCompatActivity {
+
+}
--- app/src/main/java/com/jackist/fishweather/ui/MainActivity.java
+++ app/src/main/java/com/jackist/fishweather/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package com.jackist.fishweather;
+package com.jackist.fishweather.ui;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.jackist.fishweather.R;
 
 
 public class MainActivity extends AppCompatActivity {
